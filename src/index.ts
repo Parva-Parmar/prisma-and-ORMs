8d74d6345697c8567646fc2139c823d321de3677
Parva Parmar <parva004cp@gmail.com>
2025-07-29 13:52:38 +0530

    created a client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hi there");
+import { PrismaClient } from "../node_modules/@prisma/client";
+
+const client = new PrismaClient();
+
+async function createUser(){
+    await client.user.create({
+        data: {
+            username: "parva",
+            password: "parva123",
+            age: 21,
+            city: "Valsad"
+        }
+    })
+}
+
+createUser();
